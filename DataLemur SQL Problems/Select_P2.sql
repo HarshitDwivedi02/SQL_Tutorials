7753e93dc925e4e7f5bbf2f1e4d69736e92ce052
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2023-10-11 21:57:45 +0530

    Select Problem

---
+++ DataLemur SQL Problems/Select_P2.sql
@@ -0,0 +1,18 @@
+/*
+Your given a products table, which contains data about different Microsoft Azure cloud products.
+
+Output all the data, in all the columns.
+
+products Example Output:
+product_id	product_category	product_name
+1	Analytics	Azure Databricks
+2	Analytics	Azure Stream Analytics
+4	Containers	Azure Kubernetes Service
+5	Containers	Azure Service Fabric
+6	Compute	Virtual Machines
+7	Compute	Azure Functions
+
+*/
+
+SELECT *
+FROM PRODUCTS;
